e30868d22fb8b0786859f6f22059526cc62e113f
Tushar Jolly <tushar.gts7650@gmail.com>
2024-04-12 21:16:05 +0530

    First commit

---
+++ src/components/Header/menuData.tsx
@@ -0,0 +1,187 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "About",
+    path: "/about",
+    newTab: false,
+  },
+  {
+    id: 33,
+    title: "Blog",
+    path: "/blog",
+    newTab: false,
+  },
+  {
+    id: 3,
+    title: "Support",
+    path: "/contact",
+    newTab: false,
+  },
+  {
+    id: 4,
+    title: "UPSC",
+    newTab: false,
+    submenu: [
+      {
+        id: 41,
+        title: "Prelims PYQs",
+        path: "/about",
+        newTab: false,
+      },
+      {
+        id: 42,
+        title: "UPSC Exam",
+        path: "/contact",
+        newTab: false,
+      },
+      {
+        id: 43,
+        title: "UPSC Syllabus",
+        path: "/blog",
+        newTab: false,
+      },
+      {
+        id: 44,
+        title: "UPSC Booklist",
+        path: "/blog-sidebar",
+        newTab: false,
+      },
+      {
+        id: 45,
+        title: "UPSC Calender",
+        path: "/blog-details",
+        newTab: false,
+      },
+      {
+        id: 46,
+        title: "UPSC Notification",
+        path: "/signin",
+        newTab: false,
+      },
+      {
+        id: 47,
+        title: "CDS Exam",
+        path: "/signup",
+        newTab: false,
+      },
+      {
+        id: 48,
+        title: "CDS PYQS",
+        path: "/error",
+        newTab: false,
+      },
+      {
+        id: 49,
+        title: "CAPF PYQS",
+        path: "/error",
+        newTab: false,
+      },
+      {
+        id: 50,
+        title: "All Categories",
+        path: "/error",
+        newTab: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "UPSC Notes",
+    newTab: false,
+    submenu: [
+      {
+        id: 51,
+        title: "Geography",
+        path: "/about",
+        newTab: false,
+      },
+      {
+        id: 52,
+        title: "World Geography",
+        path: "/contact",
+        newTab: false,
+      },
+      {
+        id: 53,
+        title: "Environment",
+        path: "/blog",
+        newTab: false,
+      },
+      {
+        id: 54,
+        title: "Science",
+        path: "/blog-sidebar",
+        newTab: false,
+      },
+      {
+        id: 55,
+        title: "Disaster Management",
+        path: "/blog-details",
+        newTab: false,
+      },
+      {
+        id: 56,
+        title: "Polity",
+        path: "/signin",
+        newTab: false,
+      },
+      {
+        id: 57,
+        title: "Goverence",
+        path: "/signup",
+        newTab: false,
+      },
+      {
+        id: 58,
+        title: "Internal Security",
+        path: "/error",
+        newTab: false,
+      },
+      {
+        id: 59,
+        title: "Social Justice",
+        path: "/error",
+        newTab: false,
+      },
+      {
+        id: 60,
+        title: "International Relations",
+        path: "/error",
+        newTab: false,
+      },
+      {
+        id: 61,
+        title: "Art And Culture",
+        path: "/error",
+        newTab: false,
+      },
+      {
+        id: 62,
+        title: "Ancient History",
+        path: "/error",
+        newTab: false,
+      },
+      {
+        id: 63,
+        title: "Medieval History",
+        path: "/error",
+        newTab: false,
+      },
+      {
+        id: 64,
+        title: "Modern History",
+        path: "/error",
+        newTab: false,
+      },
+    ],
+  },
+];
+export default menuData;
