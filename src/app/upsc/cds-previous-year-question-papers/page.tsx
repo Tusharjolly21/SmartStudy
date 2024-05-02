35e0aadf1820c9da5c1542c9f7ad7e9753c1e5e1
Tushar Jolly <tushar.gts7650@gmail.com>
2024-05-02 18:33:44 +0530

    2/05

--- src/app/upsc/cds-previous-year-question-papers/page.tsx
+++ src/app/upsc/cds-previous-year-question-papers/page.tsx
@@ -1,6 +1,7 @@
 import SectionTitle from "@/components/Common/SectionTitle";
 import { Metadata } from "next";
 import Link from "next/link";
+import cdsPyqData from "@/app/upsc/cds-previous-year-question-papers/pyqdata";
 
 export const metadata: Metadata = {
   title: "CDS PYQS",
@@ -21,83 +22,16 @@ const CDSPYQ = () => {
           center
         />
         <section className="grid grid-cols-1 gap-4 px-4 md:grid-cols-2 lg:grid-cols-2 lg:gap-6">
-          <Link
-            className="flex items-center justify-between rounded-lg border-4 px-6 py-6 text-sm font-medium transition-colors hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700 dark:focus-visible:ring-gray-300"
-            href="https://digital-ocean-space-bucket.blr1.digitaloceanspaces.com/cdspyqs/20241GK.pdf"
-            target="_blank"
-          >
-            CDS Previous Year Question 2024- GK-1
-          </Link>
-          <Link
-            className="flex items-center justify-between rounded-lg border-4 px-6 py-6 text-sm font-medium transition-colors hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700 dark:focus-visible:ring-gray-300"
-            href="https://digital-ocean-space-bucket.blr1.digitaloceanspaces.com/cdspyqs/CDSEI2023-compressed.pdf"
-            target="_blank"
-          >
-            CDS Previous Year Question 2023- GK-1
-          </Link>
-          <Link
-            className="flex items-center justify-between rounded-lg border-4 px-6 py-6 text-sm font-medium transition-colors hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700 dark:focus-visible:ring-gray-300"
-            href="https://digital-ocean-space-bucket.blr1.digitaloceanspaces.com/cdspyqs/CDSEII2023GK-compressed.pdf"
-            target="_blank"
-          >
-            CDS Previous Year Question 2023- GK-2
-          </Link>
-          <Link
-            className="flex items-center justify-between rounded-lg border-4 px-6 py-6 text-sm font-medium transition-colors hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700 dark:focus-visible:ring-gray-300"
-            href="https://digital-ocean-space-bucket.blr1.digitaloceanspaces.com/cdspyqs/CDSI2022.pdf"
-            target="_blank"
-          >
-            CDS Previous Year Question 2022- GK-1
-          </Link>
-          <Link
-            className="flex items-center justify-between rounded-lg border-4 px-6 py-6 text-sm font-medium transition-colors hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700 dark:focus-visible:ring-gray-300"
-            href="https://digital-ocean-space-bucket.blr1.digitaloceanspaces.com/cdspyqs/CDSII2022_11zon.pdf"
-            target="_blank"
-          >
-            CDS Previous Year Question 2022- GK-2
-          </Link>
-          <Link
-            className="hover:bg-yellow-200 flex items-center justify-between rounded-lg border-4 px-6 py-6 text-sm font-medium transition-colors focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700 dark:focus-visible:ring-gray-300"
-            href="https://digital-ocean-space-bucket.blr1.digitaloceanspaces.com/cdspyqs/CDSI2021_11zon.pdf"
-            target="_blank"
-          >
-            CDS Previous Year Question 2021- GK-1
-          </Link>
-          <Link
-            className="flex items-center justify-between rounded-lg border-4 px-6 py-6 text-sm font-medium transition-colors hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700 dark:focus-visible:ring-gray-300"
-            href="https://digital-ocean-space-bucket.blr1.digitaloceanspaces.com/cdspyqs/CDSII2021_11zon.pdf"
-            target="_blank"
-          >
-            CDS Previous Year Question 2021- GK-2
-          </Link>
-          <Link
-            className="flex items-center justify-between rounded-lg border-4 px-6 py-6 text-sm font-medium transition-colors hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700 dark:focus-visible:ring-gray-300"
-            href="https://digital-ocean-space-bucket.blr1.digitaloceanspaces.com/cdspyqs/CDSI2020GK_11zon.pdf"
-            target="_blank"
-          >
-            CDS Previous Year Question 2020- GK-1
-          </Link>
-          <Link
-            className="flex items-center justify-between rounded-lg border-4 px-6 py-6 text-sm font-medium transition-colors hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700 dark:focus-visible:ring-gray-300"
-            href="https://digital-ocean-space-bucket.blr1.digitaloceanspaces.com/cdspyqs/CDSEII20_11zon.pdf"
-            target="_blank"
-          >
-            CDS Previous Year Question 2020- GK-2
-          </Link>
-          <Link
-            className="flex items-center justify-between rounded-lg border-4 px-6 py-6 text-sm font-medium transition-colors hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700 dark:focus-visible:ring-gray-300"
-            href="https://digital-ocean-space-bucket.blr1.digitaloceanspaces.com/cdspyqs/CDSI2019_11zon.pdf"
-            target="_blank"
-          >
-            CDS Previous Year Question 2019- GK-1
-          </Link>
-          <Link
-            className="flex items-center justify-between rounded-lg border-4 px-6 py-6 text-sm font-medium transition-colors hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700 dark:focus-visible:ring-gray-300"
-            href="https://digital-ocean-space-bucket.blr1.digitaloceanspaces.com/cdspyqs/CDSII2019_11zon.pdf"
-            target="_blank"
-          >
-            CDS Previous Year Question 2019- GK-2
-          </Link>
+          {cdsPyqData.map((pyqData, index) => (
+            <Link
+              key={index}
+              className="flex items-center justify-between rounded-lg border-4 px-4 py-6 text-sm font-bold transition-colors hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700 dark:focus-visible:ring-gray-300"
+              href={pyqData.path}
+              target="_blank"
+            >
+              {pyqData.subject}
+            </Link>
+          ))}
         </section>
       </div>
     </section>
---
+++ src/app/upsc/cds-previous-year-question-papers/pyqdata.tsx
@@ -0,0 +1,60 @@
+import { Content } from "@/types/contentdata";
+
+const cdsPyqData: Content[] = [
+  {
+    id: 1,
+    subject: "CDS Previous Year Question 2024- GK-1",
+    path: "https://digital-ocean-space-bucket.blr1.digitaloceanspaces.com/cdspyqs/20241GK.pdf",
+  },
+  {
+    id: 2,
+    subject: "CDS Previous Year Question 2023- GK-1",
+    path: "https://digital-ocean-space-bucket.blr1.digitaloceanspaces.com/cdspyqs/CDSEI2023-compressed.pdf",
+  },
+  {
+    id: 3,
+    subject: "CDS Previous Year Question 2023- GK-2",
+    path: "https://digital-ocean-space-bucket.blr1.digitaloceanspaces.com/cdspyqs/CDSEII2023GK-compressed.pdf",
+  },
+  {
+    id: 4,
+    subject: "CDS Previous Year Question 2022- GK-1",
+    path: "https://digital-ocean-space-bucket.blr1.digitaloceanspaces.com/cdspyqs/CDSI2022.pdf",
+  },
+  {
+    id: 5,
+    subject: "CDS Previous Year Question 2022- GK-2",
+    path: "https://digital-ocean-space-bucket.blr1.digitaloceanspaces.com/cdspyqs/CDSII2022_11zon.pdf",
+  },
+  {
+    id: 6,
+    subject: "CDS Previous Year Question 2021- GK-1",
+    path: "https://digital-ocean-space-bucket.blr1.digitaloceanspaces.com/cdspyqs/CDSI2021_11zon.pdf",
+  },
+  {
+    id: 7,
+    subject: "CDS Previous Year Question 2021- GK-2",
+    path: "https://digital-ocean-space-bucket.blr1.digitaloceanspaces.com/cdspyqs/CDSII2021_11zon.pdf",
+  },
+  {
+    id: 8,
+    subject: "CDS Previous Year Question 2020- GK-1",
+    path: "https://digital-ocean-space-bucket.blr1.digitaloceanspaces.com/cdspyqs/CDSI2020GK_11zon.pdf",
+  },
+  {
+    id: 9,
+    subject: "CDS Previous Year Question 2020- GK-2",
+    path: "https://digital-ocean-space-bucket.blr1.digitaloceanspaces.com/cdspyqs/CDSEII20_11zon.pdf",
+  },
+  {
+    id: 10,
+    subject: "CDS Previous Year Question 2019- GK-1",
+    path: "https://digital-ocean-space-bucket.blr1.digitaloceanspaces.com/cdspyqs/CDSI2019_11zon.pdf",
+  },
+  {
+    id: 11,
+    subject: "CDS Previous Year Question 2019- GK-2",
+    path: "https://digital-ocean-space-bucket.blr1.digitaloceanspaces.com/cdspyqs/CDSII2019_11zon.pdf",
+  },
+];
+export default cdsPyqData;
